71d32352ec300c25dcda3adf79b46f3173ee08d9
JameDuncMcPh <duncmcph@ed.amdsb.ca>
2016-02-10 19:01:04 -0500

    First Touches

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Door.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Door
+    {
+        public int X;
+        public int Y;
+        public int Size;
+
+        public Door(int v1, int v2, int v3)
+        {
+            this.X = v1;
+            this.Y = v2;
+            this.Size = v3;
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class gameForm : Form
+    {
+        #region Global Varialbes
+
+        //Doors
+        Door northDoor, southDoor, westDoor, eastDoor;
+
+        //Ints
+        int doorSize = 500;
+        int playerSize = 25;
+        int playerX = 275;
+        int playerY = 500;
+
+        //Brushs
+        SolidBrush playerBrush = new SolidBrush(Color.BlueViolet);
+        #endregion
+
+        public gameForm()
+        {
+            InitializeComponent();
+        }
+
+        private void gameSetup()
+        {
+            //set the size of the doors
+            northDoor.Size = doorSize;
+            southDoor.Size = doorSize;
+            westDoor.Size = doorSize;
+            eastDoor.Size = doorSize;
+
+            //Set up the parameters of each door
+            northDoor.X = this.Width - (northDoor.Size / 2);
+            northDoor.Y = 0;
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gameForm_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
